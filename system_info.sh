7357c9daa0469c13c60f884c8eb435fc02272ba1
komal <komal.ani.bankr@email.com>
2026-09-01 21:24:01 +0530

    Add Linux System Information Script

---
+++ system_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "=========================="
+echo "   SYSTEM INFORMATION"
+echo "=========================="
+
+echo "username:"
+whoami
+
+echo "Date and Time:"
+date
+
+echo "Operating System:"
+uname -a
+echo "Current Directory:"
+pwd
+
+echo "Disk Usage:"
+df -h
+
